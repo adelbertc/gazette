29d8ccbbd45bd6c06ca11a12dde020d943294d55
Adelbert Chang <adelbertc@gmail.com>
2015-06-12 23:47:46 -0700

    Add more queries

--- src/main/scala/gazette/GazetteServer.scala
+++ src/main/scala/gazette/GazetteServer.scala
@@ -8,21 +8,17 @@ import doobie.imports._
 import doobie.util.composite._
 
 import java.sql.Date
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
-import org.http4s.UrlForm
+import org.http4s.{Response, UrlForm}
 import org.http4s.dsl.{->, /, BadRequest, BadRequestSyntax, GET, Ok, OkSyntax, POST, Root}
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.HttpService
 
-import scalaz.{Failure, NonEmptyList, Success, ValidationNel}
+import scalaz._
+import scalaz.Scalaz._
 import scalaz.concurrent.Task
-import scalaz.std.map._
-import scalaz.std.option._
-import scalaz.std.string._
-import scalaz.syntax.apply.{ToFunctorOps => _, _}
-import scalaz.syntax.show._
-import scalaz.syntax.std.option._
-import scalaz.syntax.traverse._
 
 object GazetteServer extends TaskApp {
   val transactor: Task[Transactor[Task]] = H2Transactor[Task]("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
@@ -48,6 +44,15 @@ object GazetteServer extends TaskApp {
   def selectAll: ConnectionIO[List[Todo]] =
     sql"SELECT event, category, due, tags FROM todo".query[Todo].list
 
+  def inCategory(cat: String): ConnectionIO[List[Todo]] =
+    sql"SELECT event, category, due, tags FROM todo WHERE category = ${cat}".query[Todo].list
+
+  def due(date: Date): ConnectionIO[List[Todo]] =
+    sql"SELECT event, category, due, tags FROM todo WHERE due = ${date}".query[Todo].list
+
+  def inTag(tag: String): ConnectionIO[List[Todo]] =
+    sql"SELECT event, category, due, tags FROM todo WHERE ARRAY_CONTAINS(tags, ${tag})".query[Todo].list
+
   def parseForm(map: Map[String, Seq[String]]): ValidationNel[String, Todo] = {
     val event = map.get("event").flatMap(_.headOption).toSuccess(NonEmptyList("event"))
     val category = map.get("category").flatMap(_.headOption).toSuccess(NonEmptyList("category"))
@@ -55,6 +60,30 @@ object GazetteServer extends TaskApp {
     val tags = map.get("tags").fold(List.empty[String])(_.toList)
     (event |@| category) { case (e, c) => Todo(e, c, due, tags) }
   }
+
+  def category(params: Map[String, String]): Option[Task[List[Todo]]] =
+    params.get("category").map(cat => transact(inCategory(cat)))
+
+  def currentDate: Task[Date] =
+    Task.delay {
+      val format = new SimpleDateFormat("yyyy-MM-dd")
+      Date.valueOf(format.format(Calendar.getInstance().getTime()))
+    }
+
+  def today: Task[List[Todo]] =
+    for {
+      date <- currentDate
+      rs   <- transact(due(date))
+    } yield rs
+
+  def dueOn(params: Map[String, String]): Option[Task[List[Todo]]] =
+    for {
+      stringDate <- params.get("due")
+      date       <- Util.parseDate(stringDate)
+    } yield transact(due(date))
+
+  def tag(params: Map[String, String]): Option[Task[List[Todo]]] =
+    params.get("tag").map(t => transact(inTag(t)))
 
   val service = HttpService {
     case req @ POST -> Root / "todo" =>
@@ -65,8 +94,18 @@ object GazetteServer extends TaskApp {
         }
       }
 
-    case GET -> Root / "todo" =>
-      Ok(transact(selectAll).map(_.asJson.nospaces))
+    case GET -> Root / "todo" / "today" => Ok(today.map(_.asJson.nospaces))
+
+    case req @ GET -> Root / "todo" =>
+      if (req.params.isEmpty) Ok(transact(selectAll).map(_.asJson.nospaces))
+      else {
+        val p = req.params
+        category(p).orElse(dueOn(p)).orElse(tag(p)) match {
+          case Some(r) => Ok(r.map(_.asJson.nospaces))
+          case None    => BadRequest("Query parameters must be empty or have one of {category, due, tag}.")
+        }
+      }
+
   }
 
   override def runc: Task[Unit] =
